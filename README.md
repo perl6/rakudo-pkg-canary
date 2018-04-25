4da0d4e66157fc173d61d09c98db654a7b7f9a30
Claudio Ramirez <pub.claudio@gmail.com>
2018-04-25 21:33:35 +0200

    First implementation

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rakudo-pkg-canary
-Build Canary for Rakudo Pre-releases
+
+[![Build Status](https://travis-ci.org/nxadm/rakudo-pkg-canary.svg?branch=master)](https://travis-ci.org/nxadm/rakudo-pkg-canary)
+
+This repository is not intended for end users, but as part of the Perl 6 CI
+infrastructure. See [rakudo-pkg](https://github.com/nxadm/) for user packages.
+
+This repository is used to automatically test Rakudo pre-releases. The Rakudo
+version specified in the .travis.yaml is rebuilt every 24 hours or whenever a
+push happens.
+
---
+++ docker/pkg_rakudo.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+use feature 'say';
+use File::Copy;
+use File::Path qw/remove_tree/;
+
+### Variables ###
+my $install_root = '/opt/rakudo-pkg';
+
+### Check required environment ###
+check_env( 'RAKUDO_URL') or exit 1;
+my $url = $ENV{RAKUDO_URL};
+
+### Download & compile Rakudo ###
+build() or exit 1;
+
+say "Rakudo was succesfully compiled.";
+
+### Test by installing and running it ###
+system($install_root . '/bin/perl6', '-v') == 0 or die($!);
+
+exit 0;
+
+### Functions ###
+sub build {
+    mkdir 'rakudo' or die($!);
+    # Download and unpack
+    system('wget', $url, '-O', 'rakudo.tar.gz') == 0 or return 0;
+    system('tar', 'xzf', 'rakudo.tar.gz', '-C', 'rakudo', '--strip=1') == 0
+        or return 0;
+    unlink('rakudo.tar.gz') or warn($!);
+    chdir('rakudo') or die($!);
+    # Configure
+    my @configure  = ('perl', './Configure.pl', "--prefix=$install_root",
+                      '--backend=moar', '--gen-moar');
+
+    system(@configure) == 0 or return 0;
+    system('make')     == 0 or return 0;
+    system('make', 'test') == 0 or return 0;
+
+    # make install
+    system('make', 'install') == 0 or return 0;
+    # Clean up
+    chdir('/') or die($!);
+    remove_tree('rakudo') or warn($!);
+    return 1;
+}
+
+sub check_env {
+    my @keys = @_;
+    my $error;
+    for my $key (@keys) {
+        if (! exists $ENV{$key}) {
+            say STDERR "Environment variable $key not set.";
+            $error = 1;
+        }
+    }
+    return 0 if $error;
+    return 1;
+}
