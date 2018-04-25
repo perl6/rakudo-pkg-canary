40c8f48506564034c843185daf5c8e0b2ac2db1b
Claudio Ramirez <pub.claudio@gmail.com>
2018-04-26 00:44:39 +0200

    simplify script some more

--- docker/pkg_rakudo.pl
+++ docker/pkg_rakudo.pl
@@ -6,44 +6,25 @@ use feature 'say';
 ### Variables ###
 my $install_root = '/opt/rakudo-pkg';
 my $url = $ENV{RAKUDO_URL};
-
-### Check required environment ###
 if ($url eq "") {
     say "RAKUDO_URL environment empty!";
     exit 1;
 }
 
 ### Download & compile Rakudo ###
-build() or exit 1;
-say "Rakudo was succesfully compiled.";
+mkdir '/root/rakudo' or die($!);
+system('wget', $url, '-O', 'rakudo.tar.gz') == 0 or return 0;
+system('tar', 'xzf', 'rakudo.tar.gz', '-C', 'rakudo', '--strip=1') == 0
+    or return 0;
+chdir('rakudo') or die($!);
+my @configure  =
+    ( 'perl', './Configure.pl', '--backend=moar', '--gen-moar');
+system(@configure)        == 0 or return 0;
+system('make')            == 0 or return 0;
+system('make', 'test')    == 0 or return 0;
+system('make', 'install') == 0 or return 0;
 
 ### Test by installing and running it ###
-system($install_root . '/bin/perl6', '-v') == 0 or die($!);
+system('/root/rakudo/install/bin/perl6', '-v') == 0 or die($!);
 
 exit 0;
-
-### Functions ###
-sub build {
-    mkdir 'rakudo' or die($!);
-    # Download and unpack
-    system('wget', $url, '-O', 'rakudo.tar.gz') == 0 or return 0;
-    system('tar', 'xzf', 'rakudo.tar.gz', '-C', 'rakudo', '--strip=1') == 0
-        or return 0;
-    chdir('rakudo') or die($!);
-    # Configure
-    my @configure  = (
-        'perl', './Configure.pl',
-        '--backend=moar', '--gen-moar',
-        "--prefix=$install_root"
-    );
-
-    system(@configure) == 0 or return 0;
-    system('make')     == 0 or return 0;
-    system('make', 'test') == 0 or return 0;
-
-    # make install
-    system('make', 'install') == 0 or return 0;
-    # Clean up
-    chdir('/') or die($!);
-    return 1;
-}
