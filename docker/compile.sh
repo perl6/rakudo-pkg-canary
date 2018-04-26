ca4368631f18abfca1eea1275cfe0d6fb7f6d692
Claudio Ramirez <pub.claudio@gmail.com>
2018-04-26 20:59:54 +0200

    script fixes

--- docker/compile.sh
+++ docker/compile.sh
@@ -6,13 +6,13 @@ if [ -z "$RAKUDO_URL" ]; then
     exit 1
 fi
 
-wget $RAKUDO_URL -O rakudo.tar.gz
-tar xvzf rakudo.tar.gz
+wget $RAKUDO_URL -O /var/tmp/rakudo.tar.gz
+tar xvzf /var/tmp/rakudo.tar.gz
 cd rakudo*
 perl /Configure.pl --backend=moar --gen-moar --prefix=$PREFIX
 make
 make test
 make install
-/opt/rakudo-pkg/bin/perl6
+/opt/rakudo-pkg/bin/perl6 -v 
 
 exit 0;
